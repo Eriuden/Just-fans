6b3d1044138c11ea86611c04dd94183650e11d18
Eriuden <antoinecarrette@hotmail.fr>
2023-12-04 10:47:40 +0100

    authController

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,42 @@
+const userModel = require("../models/user.model")
+const jwt = require("jsonwebtoken")
+const {signInErrors, signUpErrors} = require("../utils/errors.utils")
+
+const maxAge = 3*24*60*60*3600
+
+const createToken = (id) => {
+    return jwt.sign({id}), process.env.TOKEN_SECRET, {
+        expiresIn: maxAge
+    }
+}
+
+module.exports.signUp = async(req,res) => {
+    const {name, email, password} = req.body
+
+    try{
+        const user = await userModel.create({name,email,password})
+        res.status(201).json({user: user._id})
+    } catch(err) {
+        const errors = signUpErrors(err)
+        res.status(200).send(err)
+    }
+}
+
+module.exports.signIn = async(req,res) => {
+    const {email,password} = req.body 
+
+    try {
+        const user = await userModel.login(email,password)
+        const token = createToken(user._id)
+        res.cookie("jwt", token, {httpOnly: true, maxAge})
+        res.status(200).json({user: user._id})
+    } catch(err) {
+        const errors = signInErrors(err)
+        res.status(200).send({err})
+    }
+}
+
+module.exports.logout = (res) => {
+    res.cookie("jwt", { maxAge: 1})
+    res.redirect("/")
+}
