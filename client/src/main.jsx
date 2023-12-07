9d905684d3dd92aa9277ebc71f1ebb092d8eda0d
Eriuden <antoinecarrette@hotmail.fr>
2023-12-07 08:48:56 +0100

    paramétrages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {BrowserRouter} from "react-router-dom"
+import {Provider} from "react-redux"
+import thunk from "redux-thunk"
+import {applyMiddleware} from "redux"
+import {configureStore} from "@reduxjs/toolkit"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
