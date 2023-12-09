16e7504dc8985986b174a4205dcb0509ec16a006
Eriuden <antoinecarrette@hotmail.fr>
2023-12-09 10:37:14 +0100

    searcher

---
+++ client/src/components/FanCard.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const FanCard = (fanProps) => {
+  return (
+    <div>FanCard</div>
+  )
+}
--- client/src/components/Searcher.jsx
+++ client/src/components/Searcher.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React,{ useEffect, useState } from "react"
+import { useDispatch } from "react-redux"
+import {getAllFans} from "../redux/actions/fan.actions"
+import { useSelector } from "react-redux"
+import { FanCard } from "./FanCard"
+
 
 export const Searcher = () => {
+  const [search, setSearch] = useState("")
+  const fansData = useSelector((state)=> state.fanReducer)
+  const dispatch = useDispatch()
+
+  useEffect(()=> {
+    dispatch(getAllFans)
+  }, search)
+
+  const handleSearchChange = (e)=> {
+    setSearch(e.target.value)
+
+    const result = fansData.filter((fan)=> fan.name.toLowerCase()
+    .includes(search.toLowerCase()))
+
+    setSearch(result)
+  }
   return (
-    <div>Searcher</div>
+    <div>
+      <form action="" className="my-4">
+        <input type="text" placeholder="Entrez un nom" id="search-input"
+        onChange={handleSearchChange} value={search}/>
+      </form>
+
+      <div>
+        {search ? (
+          fansData.map((fan)=> {
+            if (fan.name.includes(search)) {
+              <FanCard fanProps={fan} key={fan._id}/>
+            }
+          })
+        ): ""}
+      </div>
+    </div>
   )
 }
