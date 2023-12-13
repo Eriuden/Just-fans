cfd74f25c20c97ae80f26384370f578dbdc6003f
Eriuden <antoinecarrette@hotmail.fr>
2023-12-13 08:28:19 +0100

    home

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,7 +1,45 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { FanCard } from '../components/FanCard'
+import { useState, useEffect } from 'react'
+import { getFan } from '../redux/actions/fan.actions'
+import { isEmpty } from '../utils'
+
 
 export const Home = () => {
+  const [loadCard, setLoadCard] = useState(false)
+  const [count, setCount] = useState(0)
+  const dispatch = useDispatch()
+  const fans = useSelector((state)=> state.fanReducer)
+
+  const loadMore = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop + 1 >
+      document.scrollingElement.scrollHeight
+    ) {
+      setLoadCard(true)
+    }
+  }
+
+  useEffect(()=> {
+    if (loadCard) {
+      dispatch(getFan(count))
+      setLoadCard(false)
+      setCount(count + 10)
+    }
+    window.addEventListener("scroll",loadMore)
+  }, [loadCard, dispatch, count])
+
   return (
-    <div>home</div>
+    <div>
+      <div>
+        <ul>
+          {isEmpty((fans[0])) &&
+          fans.map((fan)=> {
+            return <FanCard fanProps={fan} key={fan._id}/>
+          })}
+        </ul>
+      </div>
+    </div>
   )
 }
